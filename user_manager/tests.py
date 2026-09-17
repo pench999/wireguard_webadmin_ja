066db8fc80cff217803d9eca37d29d2715725a59
kudo <kudo@kudo-desk.fairway.office>
2026-09-17 13:10:43 +0900

    Fix MFA setup form submission

--- user_manager/tests.py
+++ user_manager/tests.py
@@ -1,3 +1,37 @@
+import pyotp
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from user_manager.models import UserAcl, UserMfaSettings
+
+
+class UserMfaSetupTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='vpn-user', password='Test-pass-123!')
+        UserAcl.objects.create(user=self.user, user_level=0)
+        self.client.force_login(self.user)
+
+    def test_setup_button_is_inside_single_post_form(self):
+        response = self.client.get('/user/mfa/setup/')
+
+        self.assertEqual(response.status_code, 200)
+        content = response.content.decode()
+        form_start = content.index('<form method="post">')
+        submit_button = content.index('type="submit"', form_start)
+        form_end = content.index('</form>', form_start)
+        self.assertLess(form_start, submit_button)
+        self.assertLess(submit_button, form_end)
+        self.assertEqual(content.count('<form'), 1)
+        self.assertEqual(content.count('</form>'), 1)
+
+    def test_valid_totp_enables_mfa(self):
+        self.client.get('/user/mfa/setup/')
+        secret = self.client.session['pending_mfa_totp_secret']
+
+        response = self.client.post('/user/mfa/setup/', {
+            'totp_pin': pyotp.TOTP(secret).now(),
+        })
+
+        self.assertRedirects(response, '/vpn/?setup=1', fetch_redirect_response=False)
+        settings = UserMfaSettings.objects.get(user=self.user)
+        self.assertTrue(settings.totp_enabled)
